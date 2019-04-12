8efcd07fd6fbb23b0e2499bfb43e1477a2198898
x@q.com <x@q.com>
2019-04-12 08:10:23 +0800

    处理入口页面

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React from 'react';
 
 import { HashRouter } from 'react-router-dom';
-import { Icon, TabBar, LocaleProvider } from 'antd-mobile';
+import { LocaleProvider } from 'antd-mobile';
 
 import zhCN from 'antd-mobile/lib/locale-provider/en_US';
 
-import iconfontUser from './assets/images/iconfont-user.png';
-import iconfontUserActive from './assets/images/iconfont-user-active.png';
-import iconfontHome from './assets/images/iconfont-home.png';
-import iconfontHomeActive from './assets/images/iconfont-home-active.png';
-
-import BottomTabBar from './common/BottomTabBar';
 import { MyRoute } from './common/MyRoute';
 
 import { WEB_CONTEXT } from './common/Utils';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedTab: 'home',
-    };
-  }
 
   componentDidMount() {
     let token = localStorage.getItem('__token__');
