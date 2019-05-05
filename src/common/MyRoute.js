29915c6f05871d28cdaee1ac84679958032084f0
x@q.com <x@q.com>
2019-05-05 12:45:58 +0800

    调整CustomPageRoutersConfig.js所处的目录

--- src/common/MyRoute.js
+++ src/common/MyRoute.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { customPageRoutes } from './CustomPageRoutersConfig';
+import { customPageRoutes } from '../custom/CustomPageRoutersConfig';
 
 import Login from '../components/Login';
 import My from '../components/My';
