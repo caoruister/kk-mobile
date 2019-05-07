99f7a6e64d552223e497f0766172e4ea94bafcd2
x@q.com <x@q.com>
2019-05-07 14:33:54 +0800

    注释掉一些代码

--- src/custom/DynamicSection.js
+++ src/custom/DynamicSection.js
@@ -1,22 +1,17 @@
 import React from 'react';
 
 // ****************************************************
-import Section1 from './sections/PXXXXXXXXXXXXXXXXXXXXXXXXX/Section1';
-import Section2 from './sections/PXXXXXXXXXXXXXXXXXXXXXXXXX/Section2';
+// import Section1 from './sections/PXXXXXXXXXXXXXXXXXXXXXXXXX/Section1';
+// import Section2 from './sections/PXXXXXXXXXXXXXXXXXXXXXXXXX/Section2';
 // ****************************************************
 
 class DynamicSection extends React.Component {
   render() {
     let sectionName = this.props.sectionName;
     let section = null;
-    // ----------------------------------------------------
-    if ('Section1' == sectionName) {
-      section = <Section1 />;
-    }
-    if ('Section2' == sectionName) {
-      section = <Section2 />;
-    }
-    // ----------------------------------------------------
+    //
+    // if ('Section1' == sectionName) {section = <Section1 />;}
+    // if ('Section2' == sectionName) {section = <Section2 />;}
     return <div>{section}</div>;
   }
 }
