5e0b85a6119b0201b8d65546cf5614b6441ea661
曹锐 <caoruister@gmail.com>
2019-07-23 21:01:02 +0800

    add user info

--- src/custom/UserInfo.js
+++ src/custom/UserInfo.js
@@ -11,10 +11,43 @@ var styles = {
   body: {
     header: {
       icon: {
-        width: '100%'
+        position: 'absolute',
+        top: '0',
+        left: '0',
+        zIndex: '-1',
+        cursor: 'zoom-in',
+        width: '100%',
+        opacity: '1'
       },
       avatar: {
-        icon: {}
+        display: 'flex',
+        justifyContent: 'space-start',
+        alignItems: 'center',
+        margin: '59px 20px 0 20px',
+        icon: {
+          width: '63px',
+          height: '63px'
+        },
+        name: {
+          marginLeft: '20px',
+          fontSize: '24px',
+          color: '#fff'
+        }
+      },
+      card: {
+        margin: '27px 16px 0 16px',
+        padding: '20px 19px 26px 19px',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        name: {
+          fontSize: '19px',
+          color: '#cc9e48'
+        },
+        number: {
+          fontSize: '15px',
+          color: '#fff'
+        }
       }
     },
 
@@ -36,9 +69,12 @@ class UserInfo extends React.Component {
             <img src={backgroundIcon} style={styles.body.header.icon} />
             <div style={styles.body.header.avatar}>
               <img src={avatarIcon} style={styles.body.header.avatar.icon} />
-              <div>尊贵的唐先生</div>
+              <div style={styles.body.header.avatar.name}>尊贵的唐先生</div>
             </div>
-            <div style={styles.body.header.card} />
+            <div style={styles.body.header.card}>
+              <div style={styles.body.header.card.name}>鹏程卡</div>
+              <div style={styles.body.header.card.number}>401****4546</div>
+            </div>
           </div>
           <div style={styles.body.list} />
         </div>
